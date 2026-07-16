e0a872106cd2dec81771ebdc5c03dcfb9721922d
Facundo <facundomoriconi19@gmail.com>
2026-07-16 16:29:12 -0300

    seccion 11

--- app/switches/index.tsx
+++ app/switches/index.tsx
@@ -1,10 +1,52 @@
-import { View, Text } from 'react-native';
+import ThemeCard from '@/presentation/shared/ThemeCard';
+import ThemedView from '@/presentation/shared/ThemedView';
+import ThemeSwitch from '@/presentation/shared/ThemeSwitch';
+
+import { useState } from 'react';
 
 const Switches = () => {
+
+  const [state, setState] = useState({
+    isActive: true,
+    isHungry: false,
+    isHappy: true
+  })
+
+
   return (
-    <View>
-      <Text>Switches</Text>
-    </View>
+    <ThemedView margin className='mt-2'>
+      <ThemeCard>
+        {/* <Switch
+        trackColor={{ false: '#767577', true: '#81b0ff' }}
+        thumbColor={state.isActive ? '#f5dd4b' : '#f4f3f4'}
+        ios_backgroundColor="#3e3e3e"
+        onValueChange={(value) => setState({...state, isActive: value})}
+        value={state.isActive}
+      /> */}
+
+      <ThemeSwitch
+        text='Activo'
+        value= {state.isActive}
+        onValueChange={(value)=> setState({...state, isActive: value})}
+        className='mb-4'
+      />
+
+      <ThemeSwitch
+        text='Hambriento'
+        value= {state.isHungry}
+        onValueChange={(value)=> setState({...state, isHungry: value})}
+        className='mb-4'
+      />
+
+      <ThemeSwitch
+        text='Feliz'
+        value= {state.isHappy}
+        onValueChange={(value)=> setState({...state, isHappy: value})}
+        className='mb-4'
+      />
+
+      </ThemeCard>
+    </ThemedView>
   );
 };
 export default Switches;
--- app/text-inputs/index.tsx
+++ app/text-inputs/index.tsx
@@ -1,10 +1,92 @@
-import { View, Text } from 'react-native';
+import ThemeCard from '@/presentation/shared/ThemeCard';
+import ThemedText from '@/presentation/shared/ThemedText';
+import ThemedTextInput from '@/presentation/shared/ThemedTextInput';
+import ThemedView from '@/presentation/shared/ThemedView';
+import { useState } from 'react';
+import { KeyboardAvoidingView, Platform, ScrollView } from 'react-native';
 
 const TextInputsScreen = () => {
+
+  const isIOS = Platform.OS === 'ios';
+
+
+  const [form, setForm] = useState({
+    name: '',
+    email: '',
+    phone: '',
+  })
+
   return (
-    <View>
-      <Text>TextInputsScreen</Text>
-    </View>
+    <KeyboardAvoidingView
+      behavior={ isIOS ? 'height': undefined }
+    >
+      <ScrollView>
+        <ThemedView margin>
+          <ThemeCard className='mb-5'>
+            <ThemedTextInput
+              placeholder='Nombre Completo'
+              autoCapitalize={'words'}
+              autoCorrect={false}
+              onChangeText={(text) => setForm({ ...form, name: text })}
+            />
+
+            <ThemedTextInput
+              placeholder='Correo Electronico'
+              autoCapitalize={'words'}
+              autoCorrect={false}
+              keyboardType='email-address'
+              onChangeText={(text) => setForm({ ...form, email: text })}
+            />
+
+            <ThemedTextInput
+              placeholder='Télefono'
+              autoCapitalize={'words'}
+              autoCorrect={false}
+              keyboardType='phone-pad'
+              onChangeText={(text) => setForm({ ...form, email: text })}
+            />
+
+          </ThemeCard>
+
+          <ThemeCard className='my-2'>
+            <ThemedText className='dark:text-white'>{JSON.stringify(form, null, 2)}</ThemedText>
+          </ThemeCard>
+          <ThemeCard className='my-2'>
+            <ThemedText className='dark:text-white'>{JSON.stringify(form, null, 2)}</ThemedText>
+          </ThemeCard>
+          <ThemeCard className='my-2'>
+            <ThemedText className='dark:text-white'>{JSON.stringify(form, null, 2)}</ThemedText>
+          </ThemeCard>
+          <ThemeCard className='my-2'>
+            <ThemedText className='dark:text-white'>{JSON.stringify(form, null, 2)}</ThemedText>
+          </ThemeCard>
+          <ThemeCard className='my-2'>
+            <ThemedText className='dark:text-white'>{JSON.stringify(form, null, 2)}</ThemedText>
+          </ThemeCard>
+          <ThemeCard className='my-2'>
+            <ThemedText className='dark:text-white'>{JSON.stringify(form, null, 2)}</ThemedText>
+          </ThemeCard>
+          <ThemeCard className='my-2'>
+            <ThemedText className='dark:text-white'>{JSON.stringify(form, null, 2)}</ThemedText>
+          </ThemeCard>
+
+          <ThemeCard style={{
+            marginBottom: isIOS ? 100 : 10
+          }}>
+            <ThemedTextInput
+              placeholder='Nombre Completo'
+              autoCapitalize={'words'}
+              autoCorrect={false}
+              onChangeText={(text) => setForm({ ...form, name: text })}
+            />
+          </ThemeCard>
+
+
+        </ThemedView>
+      </ScrollView>
+    </KeyboardAvoidingView>
+
+
   );
 };
 export default TextInputsScreen;
