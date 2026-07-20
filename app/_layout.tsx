3bf40bfb2cdfebd0937040322c7783ff11453c0f
Facundo <facundomoriconi19@gmail.com>
2026-07-20 14:51:44 -0300

    seccion 12

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,50 +1,49 @@
-import {
-  DarkTheme,
-  DefaultTheme,
-  ThemeProvider,
-} from '@react-navigation/native';
 import { Stack } from 'expo-router';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 import 'react-native-reanimated';
 
 import { allRoutes } from '@/constants/Routes';
-import { useColorScheme } from '@/hooks/use-color-scheme';
 import { useThemeColor } from '@/hooks/use-theme-color';
 
+import { ThemeChangerProvider } from '@/presentation/context/ThemedChangerContext';
 import '../global.css';
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
+
   const backgroundColor = useThemeColor({}, 'background');
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack
-        screenOptions={{
-          headerShadowVisible: false,
-          contentStyle: {
-            backgroundColor: backgroundColor,
-          },
-          headerStyle: {
-            backgroundColor: backgroundColor,
-          },
-        }}
-      >
-        <Stack.Screen name="index" options={{ title: 'Inicio' }} />
 
-        {
-          allRoutes.map(route => (
-            <Stack.Screen
-              key={route.name}
-              name= {route.name}
-              options={{
-                title: route.title
-              }}
-            />
-          ))
-        }
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <ThemeChangerProvider>
+        <Stack
+          screenOptions={{
+            headerShadowVisible: false,
+            contentStyle: {
+              backgroundColor: backgroundColor,
+            },
+            headerStyle: {
+              backgroundColor: backgroundColor,
+            },
+          }}
+        >
+          <Stack.Screen name="index" options={{ title: 'Inicio' }} />
 
-      </Stack>
-    </ThemeProvider>
+          {
+            allRoutes.map(route => (
+              <Stack.Screen
+                key={route.name}
+                name={route.name}
+                options={{
+                  title: route.title,
+                  headerShown: !route.title.includes('Slides'),
+                }}
+              />
+            ))
+          }
+        </Stack>
+      </ThemeChangerProvider>
+    </GestureHandlerRootView >
   );
 }
--- app/themes/index.tsx
+++ app/themes/index.tsx
@@ -1,10 +1,60 @@
-import { View, Text } from 'react-native';
+import { useThemeChangerContext } from '@/presentation/context/ThemedChangerContext';
+import ThemeCard from '@/presentation/shared/ThemeCard';
+import ThemedView from '@/presentation/shared/ThemedView';
+import ThemeSwitch from '@/presentation/shared/ThemeSwitch';
+import { useState } from 'react';
 
 const ThemesScreen = () => {
+
+  const { toggleTheme, currentTheme, setSystemTheme, isSystemTheme } = useThemeChangerContext();
+
+  //const { colorScheme, setColorScheme } = useColorScheme();
+
+  const [darkModeSettings, setDarkModeSettings] = useState({
+    darkMode: currentTheme === 'dark',
+    systemMode: isSystemTheme
+  })
+
+  const setDarkMode = (value: boolean) => {
+
+    //setColorScheme(value ? 'dark' : 'light')
+    toggleTheme();
+
+    setDarkModeSettings({
+      darkMode: value,
+      systemMode: false
+    })
+  }
+
+  const setSystemMode = (value: boolean) => {
+
+    if (value) {
+      setSystemTheme()
+    }
+    setDarkModeSettings({
+      darkMode: darkModeSettings.darkMode,
+      systemMode: value
+    })
+  }
+
   return (
-    <View>
-      <Text>ThemesScreen</Text>
-    </View>
+    <ThemedView>
+      <ThemeCard className='mt-5'>
+        <ThemeSwitch
+          text='Dark Mode'
+          className='mb-5'
+          value={darkModeSettings.darkMode}
+          onValueChange={setDarkMode}
+        />
+
+        <ThemeSwitch
+          text='System Mode'
+          className='mb-5'
+          value={darkModeSettings.systemMode}
+          onValueChange={setSystemMode}
+        />
+      </ThemeCard>
+    </ThemedView>
   );
 };
 export default ThemesScreen;
